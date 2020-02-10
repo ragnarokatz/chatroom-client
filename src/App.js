2062a26c2a1fc87f1b3d3784169c2bf2b0310ce0
Bowragon <ragnarokatz@gmail.com>
2020-02-10 00:07:33 -0500

    simplify main page

--- src/App.js
+++ src/App.js
@@ -3,35 +3,27 @@ import logo from "./logo.svg";
 import "./App.css";
 
 var socket = require("socket.io-client")(process.env.REACT_APP_CHATROOM_SERVER_URL);
-socket.on("connect", function() {
+socket.on("connect", function () {
   console.log("connected to server");
 });
 
-socket.on("event", function(data) {
+socket.on("event", function (data) {
   console.log("event received");
 });
 
-socket.on("disconnect", function() {
+socket.on("disconnect", function () {
   console.log("disconnected from server");
 });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <ul id="messages"></ul>
+        <form action="">
+          <input id="m" autoComplete="off" /><button>Send</button>
+        </form>
+      </div>
     </div>
   );
 }
